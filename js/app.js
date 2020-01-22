8d816b5ef8589d4bf754f4cda9a2bf0b1baf368d
coleman <coleman@hidt>
2020-01-21 23:02:44 -0300

    Add: validation

--- js/app.js
+++ js/app.js
@@ -6,29 +6,55 @@
 // const valorA = true;
 // const valorB = false;
 
+console.log('');
+
 document.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	let valueA = document.getElementById('valueA').value;
-
 	let valueB = document.getElementById('valueB');
+
 	let typeOperation = document.getElementById('typeOperation');
 
+	let resultOperation = 0;
+
+	// valueA = tienen nurmero = true
+	// valueA = vacio = false
+	if (!valueA || !valueB.value) {
+		alert('El campo es requerido');
+		return;
+	}
+	if (typeOperation.value == 'division' && valueB.value == 0) {
+		alert('No se puede divider entre 0');
+		return;
+	}
+
+	if (!/^([0-9])*$/.test(valueA) || !/^([0-9])*$/.test(valueB.value)) {
+		alert('Ingrese solo número');
+		return;
+	}
+
 	if (typeOperation.value == 'sum') {
-		console.log(parseInt(valueA) + parseInt(valueB.value));
+		resultOperation = parseInt(valueA) + parseInt(valueB.value);
 	}
 
 	if (typeOperation.value == 'subtraction') {
-		console.log(parseInt(valueA) - parseInt(valueB.value));
+		resultOperation = parseInt(valueA) - parseInt(valueB.value);
 	}
 
 	if (typeOperation.value == 'multiplication') {
-		console.log(parseInt(valueA) * parseInt(valueB.value));
+		resultOperation = parseInt(valueA) * parseInt(valueB.value);
 	}
 
 	if (typeOperation.value == 'division') {
-		console.log(parseInt(valueA) / parseInt(valueB.value));
+		resultOperation = parseInt(valueA) / parseInt(valueB.value);
 	}
+
+	document.getElementById('type-operation').innerHTML = typeOperation.value;
+
+	document.getElementById('result-operation').innerHTML = resultOperation;
+
+	console.log('el resultado de la operacion');
 });
 
 // No recomendable
